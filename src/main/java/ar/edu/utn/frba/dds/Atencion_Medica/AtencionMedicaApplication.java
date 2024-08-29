fb302df21677529ac5dd4085cf7b2b9e56554e55
Sichermatias <Sicher2001@gmail.com>
2024-08-28 21:53:24 -0300

    Configuracion Inicial del Proyecto con SpringBoot

---
+++ src/main/java/ar/edu/utn/frba/dds/Atencion_Medica/AtencionMedicaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.Atencion_Medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtencionMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtencionMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frba/dds/Atencion_Medica/AtencionMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.Atencion_Medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtencionMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
